94365a40609bd5d79cd63becbe02f17e64c98322
JaonHax <36414836+JaonHax@users.noreply.github.com>
2018-05-29 11:35:14 -0500

    Update basic_bot.py

--- examples/basic_bot.py
+++ examples/basic_bot.py
@@ -6,7 +6,7 @@ description = '''An example bot to showcase the discord.ext.commands extension
 module.
 
 There are a number of utility commands being showcased here.'''
-bot = commands.Bot(command_prefix='?', description=description)
+bot = commands.Bot(command_prefix='\', description=description)
 
 @bot.event
 async def on_ready():
@@ -16,37 +16,22 @@ async def on_ready():
     print('------')
 
 @bot.command()
-async def add(left : int, right : int):
-    """Adds two numbers together."""
-    await bot.say(left + right)
-
-@bot.command()
 async def roll(dice : str):
-    """Rolls a dice in NdN format."""
+    """Rolls a dice in NdX format."""
     try:
         rolls, limit = map(int, dice.split('d'))
     except Exception:
-        await bot.say('Format has to be in NdN!')
+        await bot.say('Format has to be in NdX!')
         return
 
     result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
     await bot.say(result)
 
-@bot.command(description='For when you wanna settle the score some other way')
-async def choose(*choices : str):
-    """Chooses between multiple choices."""
-    await bot.say(random.choice(choices))
+@bot.command
+async def money():
+    """Shows user's money"""
+    
 
-@bot.command()
-async def repeat(times : int, content='repeating...'):
-    """Repeats a message multiple times."""
-    for i in range(times):
-        await bot.say(content)
-
-@bot.command()
-async def joined(member : discord.Member):
-    """Says when a member joined."""
-    await bot.say('{0.name} joined in {0.joined_at}'.format(member))
 
 @bot.group(pass_context=True)
 async def cool(ctx):
